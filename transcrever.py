9ec5f3e172ec1c9ab2c4027de825ddb6f5302c49
Jackoki <kenji.inoue@hotmail.com>
2026-05-08 10:57:24 -0300

    Inserção de pastas a partir do .gitkeep, melhoria na legibilidade do código e manutenção divindindo as funções

--- transcrever.py
+++ transcrever.py
@@ -1,36 +1,63 @@
-import whisper
 import os
 
-PASTA_VIDEOS = "videos"
-PASTA_TRANSCRICOES = "transcricoes"
+NOME_PASTA_DE_VIDEOS = "videos"
+NOME_PASTA_DE_TRANSCRICOES = "transcricoes"
 
-os.makedirs(PASTA_VIDEOS, exist_ok=True)
-os.makedirs(PASTA_TRANSCRICOES, exist_ok=True)
+def preparar_pastas():
+    os.makedirs(NOME_PASTA_DE_VIDEOS, exist_ok=True)
+    os.makedirs(NOME_PASTA_DE_TRANSCRICOES, exist_ok=True)
 
-print("Carregando modelo...")
-model = whisper.load_model("base")
 
-videos = [f for f in os.listdir(PASTA_VIDEOS) if f.endswith(".mp4")]
+def carregar_modelo():
+    import whisper
+    return whisper.load_model("base")
 
-if not videos:
-    print("Nenhum vídeo encontrado.")
-    exit()
 
-for video in videos:
-    caminho_video = os.path.join(PASTA_VIDEOS, video)
+def obter_videos(pasta):
+    return [f for f in os.listdir(pasta) if f.endswith(".mp4")]
 
-    print(f"\nTranscrevendo: {video}")
 
-    resultado = model.transcribe(caminho_video)
+def transcrever_video(modelo, caminho_video):
+    return modelo.transcribe(caminho_video)
 
+
+def salvar_transcricao(texto, caminho_saida):
+    with open(caminho_saida, "w", encoding="utf-8") as f:
+        f.write(texto)
+
+
+def processar_video(modelo, pasta_videos, pasta_transcricoes, video):
+    caminho_video = os.path.join(pasta_videos, video)
+
+    resultado = transcrever_video(modelo, caminho_video)
     texto = resultado["text"]
 
     nome_txt = os.path.splitext(video)[0] + ".txt"
-    caminho_txt = os.path.join(PASTA_TRANSCRICOES, nome_txt)
+    caminho_txt = os.path.join(pasta_transcricoes, nome_txt)
 
-    with open(caminho_txt, "w", encoding="utf-8") as f:
-        f.write(texto)
+    salvar_transcricao(texto, caminho_txt)
 
     print(f"Transcrição salva: {nome_txt}")
 
-print("\nFinalizado.")
+
+def main():
+    print("Carregando modelo e pastas...")
+
+    preparar_pastas()
+    model_whisper = carregar_modelo()
+
+    videos = obter_videos(NOME_PASTA_DE_VIDEOS)
+
+    if not videos:
+        print("Nenhum vídeo na pasta de vídeos")
+        return
+
+    for video in videos:
+        print(f"\nTranscrevendo: {video}")
+        processar_video(model_whisper, NOME_PASTA_DE_VIDEOS, NOME_PASTA_DE_TRANSCRICOES, video)
+
+    print("\nFinalizado.")
+
+
+if __name__ == "__main__":
+    main()
